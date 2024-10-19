6bf9c63d87273bf8640c8e386f839faddc8724c6
willz-b <wbarrerao@academia.usbbog.edu.co>
2024-10-19 16:01:54 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import router from "./src/router.js"; // Importa el enrutador
+
+const app = express();
+
+// nueva 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static("public"));
+
+//Establece el motor de plantillas
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// Carga la página de inicio
+app.get("/", (req, res) => {
+  res.render("inicio", {});
+});
+
+// Usa el enrutador para las rutas relacionadas con estudiantes
+app.use("/", router);
+
+// run server 
+
+const PORT = process.env.PORT || 3600;
+app.listen(PORT, () => {
+  console.log(`Servidor en ejecución en http://127.0.0.1:${PORT}`);
+});
